cd8dfd8acf39b0e45e36fa2af8ee5c427d293f1e
prathamesh magar <prathameshma1@gmail.com>
2024-01-16 00:47:19 +0530

    pohot

---
+++ tk11.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Prathamesh Magar")
+
+root.geometry("655x455")
+
+widget = Button(root,text="Click me please")
+widget.pack()
+
+
+root.mainloop()
