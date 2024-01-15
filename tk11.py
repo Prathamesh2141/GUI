ce9f5341ec24c6c8cd5ac7ba45108ee8dd2f905d
prathamesh magar <prathameshma1@gmail.com>
2024-01-16 00:58:25 +0530

    ok done tkinter event handling

--- tk11.py
+++ tk11.py
@@ -1,13 +1,16 @@
 from tkinter import *
 
+def pram(event):
+    print(f"You clicked on button {event.x}, {event.y}")
+
 root = Tk()
 
 root.title("Prathamesh Magar")
 
 root.geometry("655x455")
 
-widget = Button(root,text="Click me please")
+widget = Button(root, text="Click me please")
 widget.pack()
-
-
+widget.bind('<Button-1>', pram)  # Corrected event sequence
+widget.bind('<Double-1>', quit)
 root.mainloop()
